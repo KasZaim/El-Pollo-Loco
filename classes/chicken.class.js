bb841ca23cbc6a9be54dac3f9844681e3299ee55
Kaser Erazer <kaser@live.de>
2023-06-12 13:10:19 +0200

    first commit

---
+++ classes/chicken.class.js
@@ -0,0 +1,8 @@
+class chicken extends MovableObject{
+    
+    constructor(){
+        super().loadImg('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+        this.x = 200 + Math.random() * 500;
+    }
+
+}
