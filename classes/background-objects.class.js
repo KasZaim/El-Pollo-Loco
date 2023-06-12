8be1f011554bd9ea1a69c71cf65fa2b487c95cbc
Kaser Erazer <kaser@live.de>
2023-06-12 15:07:05 +0200

    $*

---
+++ classes/background-objects.class.js
@@ -0,0 +1,9 @@
+class BackgroundObjects extends MovableObject{
+    width = 720;
+    height = 400;
+    constructor(imagePath, x){
+        super().loadImg(imagePath);
+        this.x = x;
+        this.y = 480 - this.height;
+    }
+}
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,37 +1,47 @@
 class World {
     ctx;
-    canvas; 
+    canvas;
     character = new Character();
     enemies = [
         new chicken(),
         new chicken(),
         new chicken(),
-    ]
+    ];
     clouds = [
         new cloud()
-    ]
+    ];
+    backgroundObjects = [
+        new BackgroundObjects('img/5_background/layers/3_third_layer/1.png', 0)
+    ];
 
-    constructor(canvas){
-        this.canvas =  canvas; //übergibt der canvas; variable den parameter canvas
+    constructor(canvas) {
+        this.canvas = canvas; //übergibt der canvas; variable den parameter canvas
         this.ctx = canvas.getContext('2d');
         this.draw();
     }
 
 
-    
+
     draw() {
         let self = this;
-        this.ctx.clearRect (0, 0, this.canvas.width,this.canvas.height) // Canvas wird entleert
-        
-        this.ctx.drawImage(this.character.img,this.character.x,this.character.y,this.character.width ,this.character.height)
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img,enemy.x,enemy.y,enemy.width ,enemy.height)
-        });
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img,cloud.x,cloud.y,cloud.width ,cloud.height)
-        });
-        requestAnimationFrame(function(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height) // Canvas wird entleert
+
+        this.addToMap(this.character) // 'this.' weil die funktion außerhalb von draw() ist
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.backgroundObjects);
+
+        requestAnimationFrame(function () {
             self.draw();
         })
+    }
+    addObjectsToMap(objects){
+        objects.forEach(object => {
+            this.addToMap(object);
+        });
+
+    }
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)
     }
 }
