2a86d8e4d420c54f0a16868f9939cceeb1a6a9c7
Kaser Erazer <kaser@live.de>
2023-06-28 20:18:37 +0200

    $*

--- classes/throwable-objects.js
+++ classes/throwable-objects.js
@@ -1,20 +1,29 @@
 class ThrowableObjects extends MovableObject{
 
+    IMAGES = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+    ]
 
     constructor(x , y){
         super().loadImg('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
+        this.loadImages(this.IMAGES);
         this.x = x;
         this.y = y;
-        this.height = 80;
-        this.width = 80;
+        this.height = 90;
+        this.width = 90;
         this.throw();
     }
     
     throw(){
-        this.speedY = 30;
+        this.speedY = 20;
         this.applyGravity();
         setInterval(() => {
-            this.x += 10;
-        }, 25);
+            this.x += 20;
+            this.playAnimation(this.IMAGES)
+
+        }, 20);
     }
 }
